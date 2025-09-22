b0829dab7da4b92521a2db01ea55054e673ca6c1
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2025-09-21 22:57:15 -0400

    Add thread ping-pong benchmark, reorganize stats.

---
+++ include/01_context_switch.h
@@ -0,0 +1,10 @@
+#ifndef MICROBENCH_01_CONTEXT_SWITCH_H
+#define MICROBENCH_01_CONTEXT_SWITCH_H
+
+#include "stats.h"
+
+calibrated_stats contextswitch_setup(int iterations);
+void * contextswitch_ping(void * ptr);
+void * contextswitch_pong(void * ptr);
+void contextswitch_pingpong(int *store);
+#endif
---
+++ src/01_context_switch.c
@@ -0,0 +1,128 @@
+#include <semaphore.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <x86intrin.h>
+#include <emmintrin.h>
+
+#include "stats.h"
+#include "01_context_switch.h"
+
+// The semaphore to ping/pong on.
+static sem_t sem;
+// To make the threads synchronize their starts.
+static pthread_barrier_t barrier;
+
+static int n_test;
+static long long *starts;
+static long long *ends;
+
+calibrated_stats contextswitch_setup(int iterations) {
+	n_test = iterations;
+	// Setup the semaphore for inter-thread (but not inter-process) sharing
+	sem_init(&sem, 0, 0);
+	pthread_barrier_init(&barrier, NULL, 2);
+	starts = malloc(sizeof(long long) * n_test);
+	ends = malloc(sizeof(long long) * n_test);
+	// For holding differences. The differences will be much smaller than INTMAX.
+	int *times = malloc(sizeof(int) * n_test);
+	// This one is for the calibration run. It doesn't need explicit start and end arrays;
+	// it will only be running on one thread.
+	int *times_calib = malloc(sizeof(int) * n_test);
+
+	// Start the experiment!
+	pthread_t pingThread, pongThread;
+	pthread_create(&pingThread, NULL, &contextswitch_ping, NULL);
+	pthread_create(&pongThread, NULL, &contextswitch_pong, NULL);
+
+	// Wait for it to finish before starting the calibration run.
+	pthread_join(pingThread, NULL);
+	pthread_join(pongThread, NULL);
+
+	for (int i = 0; i < n_test; i++) {
+		times[i] = ends[i] - starts[i];
+	}
+
+	free(starts);
+	free(ends);
+	starts = NULL;
+	ends = NULL;
+
+	contextswitch_pingpong(times_calib);
+
+	// Skip the first 10th of the tests as "warmup"
+	int n_skip = n_test/10;
+	int n_valid_test = n_test - n_skip;
+	calibrated_stats s = {
+		int_stats(times_calib + n_skip, n_valid_test),
+		int_stats(times + n_skip, n_valid_test)
+	};
+
+	free(times);
+	free(times_calib);
+	times = NULL;
+	times_calib = NULL;
+
+	return s;
+}
+
+void * contextswitch_ping(void * ptr) {
+	(void)ptr;
+    unsigned int tsc_aux;
+	unsigned long long tsc;
+
+	for (long i = 0; i < n_test; i++) {
+		// Synchronize up with the other thread to have a consistent start point.
+		pthread_barrier_wait(&barrier);
+		// Let the other thread wait on the semaphore.
+		usleep(1000);
+		// Start the clock!
+		tsc = __rdtscp(&tsc_aux);
+		// For consistent instruction ordering.
+		_mm_lfence();
+		// Wakeup the other thread.
+		sem_post(&sem);
+		// Let the other thread get our message, please.
+		usleep(1000);
+		starts[i] = tsc;
+	}
+	return NULL;
+}
+
+void * contextswitch_pong(void * ptr) {
+	(void)ptr;
+    unsigned int tsc_aux;
+	unsigned long long tsc;
+
+	for (long i = 0; i < n_test; i++) {
+		// Synchronize up with the other thread to have a consistent start point.
+		pthread_barrier_wait(&barrier);
+		// Wait on the semaphore.
+		sem_wait(&sem);
+		// Stop the clock.
+		tsc = __rdtscp(&tsc_aux);
+		// For consistent instruction ordering.
+		_mm_lfence();
+		ends[i] = tsc;
+	}
+	return NULL;
+}
+
+void contextswitch_pingpong(int *store) {
+	unsigned int tsc_aux;
+	unsigned long long tsc_start;
+	unsigned long long tsc_end;
+	for (long i = 0; i < n_test; i++) {
+		tsc_start = __rdtscp(&tsc_aux);
+		_mm_lfence();
+		// These should return instantly without incurring a context switch.
+		sem_post(&sem);
+		// We are incurring a function call overhead here. This is fine; the real thing
+		// has a usleep() function call to match. And this way we get to sample the 
+		// consequences of performing sem_wait's cleanup.
+		sem_wait(&sem);
+		tsc_end = __rdtscp(&tsc_aux);
+		_mm_lfence();
+		store[i] = tsc_end - tsc_start;
+	}
+}
