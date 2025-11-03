12741207dbaada1ec8b715336f47c833bdefab10
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2025-11-03 02:20:47 -0500

    Add branch misprediction

---
+++ include/05_branch_mispredict.h
@@ -0,0 +1,8 @@
+#ifndef MICROBENCH_05_BRANCH_MISPREDICT
+#define MICROBENCH_05_BRANCH_MISPREDICT
+
+#include "stats.h"
+
+calibrated_stats mispredict(int iterations);
+
+#endif
---
+++ src/05_branch_mispredict.c
@@ -0,0 +1,31 @@
+#include "stats.h"
+#include <x86intrin.h>
+#include <emmintrin.h>
+
+__attribute__((optimize("Og"))) int measure_branches(int rounds, int threshold) {
+	long long start, end;
+	unsigned int tsc_aux;
+	for (int i = 0; i < rounds; i++) {
+		start = __rdtscp(&tsc_aux);
+		_mm_lfence();
+		if (i >= threshold) {
+			end = __rdtscp(&tsc_aux);
+			_mm_lfence();
+		}
+	}
+	return end - start;
+}
+
+calibrated_stats mispredict(int iterations) {
+	int *times = malloc(sizeof(int) * iterations);
+	int *times_calib = malloc(sizeof(int) * iterations);
+	for (int i = 0; i < iterations; i++) {
+		times[i] = measure_branches(100, 99);
+		times_calib[i] = measure_branches(100, 0);
+	}
+	calibrated_stats s = {
+		.calibration = int_stats(times_calib, iterations),
+		.measurement = int_stats(times, iterations)
+	};
+	return s;
+}
