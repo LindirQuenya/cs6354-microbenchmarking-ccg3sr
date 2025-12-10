96c4b9862730886ce75093dd1563e75ee22c4aca
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2025-12-09 21:29:27 -0500

    Use clflush for dram instead of filling L3 manually

--- src/09_dram_latency.c
+++ src/09_dram_latency.c
@@ -1,31 +1,17 @@
 #include <x86intrin.h>
 #include <emmintrin.h>
-#include <string.h>
 
 #include "09_dram_latency.h"
 #include "stats.h"
 #include "opt.h"
 #include "cache.h"
 
-// If these aren't marked volatile the compiler optimizes out the cache-filling
-// operations.
-volatile __attribute__((aligned(64))) unsigned char l3_arr[L3_CACHE_SIZE];
-
 volatile
     __attribute__((aligned(64))) unsigned char dram_target_cacheline[LINE_SIZE];
-
-void setup_dramlat(void) {
-    _mm_mfence();
-    memset((void *)l3_arr, 'a', L3_CACHE_SIZE);
-    l3_arr[L3_CACHE_SIZE - 1] = 0;
-    __cpuid();
-}
 
 NOINLINE long measure_dram_calib(void) {
     long long start, end;
     unsigned int tsc_aux;
-    // Load the target cache line into L1d,L2,L3.
-    dram_target_cacheline[0] = 'b';
 
     _mm_mfence();
     start = __rdtscp(&tsc_aux);
@@ -44,10 +30,9 @@ NOINLINE long measure_dram(void) {
     long long start, end;
     unsigned int tsc_aux;
     register unsigned char target;
-    // Load the target cache line into L1d,L2,L3.
-    dram_target_cacheline[0] = 'b';
-    // But now fill up L3 (and thus also L2+L1d) with a different array.
-    for (int i = 0; l3_arr[i] != 0; i++);
+
+    // Explicitly evict the target from the cache.
+    _mm_clflush((void *)dram_target_cacheline);
 
     _mm_mfence();
     start = __rdtscp(&tsc_aux);
@@ -68,8 +53,7 @@ calibrated_stats dram_latency(int iterations) {
     int i;
     int *times = malloc(sizeof(int) * iterations);
     int *times_calib = malloc(sizeof(int) * iterations);
-    // Prep the array.
-    setup_dramlat();
+
     for (i = 0; i < iterations; i++) {
         times_calib[i] = measure_dram_calib();
     }
